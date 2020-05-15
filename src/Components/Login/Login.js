a24020ec8e9378310ec3b1b2d41c273c7a1e51e9
Noah Menikefs <noah.menikefs@Noahs-MacBook-Air.local>
2020-05-15 15:51:53 -0400

    push

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import Logo from '../../logo512.png';
 import './Login.css';
 
 
@@ -15,24 +16,32 @@ class Login extends React.Component{
 
 	render(){
 		return(
-			
 			<div>
-				<Form className="login-form">
-					<h1 id="loginTitle">Login</h1>
-					<Form.Group controlId="formBasicEmail">
-					    <Form.Control type="email" placeholder="Email" />
-					  </Form.Group>
+				<div id='loginHeader'>
+					<img style={{paddingTop:'5px', height:90, width:90}} alt='Logo' src={Logo}/> 
+					<h1 id='title'>MeniSked</h1>
+				</div>
+				<div id='loginBody'>
+					<Form className="login-form">
+						<h1 id="loginTitle">Login</h1>
+						<Form.Group controlId="formBasicEmail">
+						    <Form.Control type="email" placeholder="Email" />
+						  </Form.Group>
 
-					<Form.Group controlId="formBasicPassword">
-						<Form.Control type="password" placeholder="Password" />
-					</Form.Group>
-					<Button onClick={() => console.log('login click')} id='loginButton' variant="primary" type="submit">
-						Login
-					</Button>
-					<Form.Group>
-						<Form.Label onClick={() => console.log('forgot password click')} id="pLabel">Forgot Password?</Form.Label>
-					</Form.Group>
-				</Form>
+						<Form.Group controlId="formBasicPassword">
+							<Form.Control type="password" placeholder="Password" />
+						</Form.Group>
+						<Button onClick={() => console.log('login click')} id='loginButton' variant="primary" type="submit">
+							Login
+						</Button>
+						<Form.Group>
+							<Form.Label onClick={() => console.log('forgot password click')} id="pLabel">Forgot Password?</Form.Label>
+						</Form.Group>
+					</Form>
+				</div>
+				<div id='loginFooter'>
+					
+				</div>
 			</div>
 
 		);
