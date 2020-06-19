cd13f222bead498558dfc3941b072628b499725d
Noah Menikefs <nmenikefs@gmail.com>
2020-06-19 15:11:37 -0400

    push

--- src/Components/PDF/MyDocument.js
+++ src/Components/PDF/MyDocument.js
@@ -12,32 +12,31 @@ const styles = StyleSheet.create({
   header: {
     fontSize: 12,
     marginBottom: 10,
-    marginTop: 5,
     textAlign: 'center',
     color: 'grey',
   },
   title: {
-    fontSize: 24,
+    fontSize: 20,
     textAlign: 'center',
     marginBottom: 5,
   },
   weekday: {
     marginTop: 5,
     marginBottom: 3,
-    fontSize: 14,
+    fontSize: 12,
     alignSelf: 'center'
   },
   table: { 
     display: "table", 
     width: "auto", 
     borderStyle: "solid", 
-    borderWidth: 1, 
+    borderWidth: 0.75, 
     borderRightWidth: 0, 
     borderBottomWidth: 0
   }, 
   tableBody:{
-       paddingRight: 10,
-       paddingLeft: 10 
+       paddingRight: 5,
+       paddingLeft: 5 
   },
   tableRow: { 
     margin: "auto", 
@@ -48,14 +47,20 @@ const styles = StyleSheet.create({
     borderStyle: "solid", 
     borderWidth: 1, 
     borderLeftWidth: 0, 
-    height: 84,
+    height: 128,
     borderTopWidth: 0 
   }, 
   tableCell: { 
     marginTop: 1,  
-    fontSize: 10,
-    paddingLeft: 3
-  } 
+    fontSize: 8,
+    paddingLeft: 5,
+    borderStyle: "solid",  
+    borderBottomWidth: 0.5
+  },
+  tableCellList: {   
+    fontSize: 8,
+    paddingLeft: 1
+  }  
 
 });
 // Create Document Component
@@ -67,7 +72,7 @@ class MyDocument extends React.Component{
   }
 
   render(){
-    const {user, type, dateContext, holiDays, personalDays, entries, callList, callSked} = this.props;
+    const {user, type, dateContext, holiDays, personalDays, entries, callList, callSked, sked} = this.props;
     let firstDay = dateContext.startOf('month').format('d'); //Day of week 0-6
     let daysInMonth = dateContext.daysInMonth();
     let ctr = 1;
@@ -81,6 +86,9 @@ class MyDocument extends React.Component{
 
     if (callSked){
       listType = [...callSked];
+    }
+    else if (sked){
+      listType = [...sked];
     }
     else {
       listType = [...personalDays];
@@ -106,6 +114,9 @@ class MyDocument extends React.Component{
               for (let x = 0; x < entries.length; x++){
                 if (entries[x].id === listType[m].id){
                   name = entries[x].name;
+                  if (listType[m].name){
+                    name = name + ' ' + listType[m].name;
+                  }
                   break;
                 }
               }
@@ -119,7 +130,7 @@ class MyDocument extends React.Component{
                 }
               }
 
-              today.push(<Text key={m} style={styles.tableCell}>{name}</Text>);
+              today.push(<Text key={m} style={styles.tableCellList}>{name}</Text>);
             }
           }
 
@@ -143,7 +154,7 @@ class MyDocument extends React.Component{
 
     return (
       <Document>
-          <Page orientation="landscape" size="A4" style={styles.page}>
+          <Page size="A4" style={styles.page}>
             <Text style={styles.header}>  
               {dept +', '+type+' Schedule'}
             </Text>
@@ -151,7 +162,7 @@ class MyDocument extends React.Component{
               {dateContext.format('MMMM Y')}
             </Text>
             <Text style={styles.weekday}>
-            Sunday                  Monday                 Tuesday              Wednesday              Thursday                 Friday                 Saturday
+            Sunday             Monday            Tuesday         Wednesday         Thursday            Friday            Saturday
             </Text>
             <View style={styles.tableBody}> 
               <View style={styles.table}> 
