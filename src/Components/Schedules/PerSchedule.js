bd987d258951823ee932e1248e0c674d1ab46af0
Noah Menikefs <nmenikefs@gmail.com>
2020-08-13 14:35:58 -0400

    push

--- src/Components/Schedules/PerSchedule.js
+++ src/Components/Schedules/PerSchedule.js
@@ -38,7 +38,8 @@ class PerSchedule extends React.Component{
 			render: false,
 			pending: [],
 			loaded: false,
-			depts: []
+			depts: [],
+			stamp: ''
 		}
 	}
 
@@ -496,9 +497,12 @@ class PerSchedule extends React.Component{
 		this.loadNewDays(today);
 	}
 	
+	hoverSpan = () => {
+		this.setState({stamp: moment().format("YYYY-MM-DD h:mm:ss a")})
+	}
 
 	render(){
-		const {show, dateContext, activeDocs, docIndex, entries, entryIndex, holiDays, nrHolidayList, render, personalDays, pending, loaded, depts} = this.state;
+		const {show, dateContext, activeDocs, docIndex, entries, entryIndex, holiDays, nrHolidayList, render, personalDays, pending, loaded, depts, stamp} = this.state;
 		const {user, today, callList} = this.props;
 
 		if (user.id && !loaded){
@@ -678,8 +682,8 @@ class PerSchedule extends React.Component{
 					<Calendar pending={pending} entries={entries} callList={callList} personalDays={personalDays} holiDays={holiDays} type="Personal" dateContext={dateContext} today={today} style={style} onDayClick={(e,day) => this.onDayClick(e,day)}/>
 				</div>
 				<div className="bottom">
-					<Col id='downloadLink'><PDFDownloadLink onMouseOver={() => console.log('hover')} document={<MyDocument stamp={moment().format("YYYY-MM-DD h:mm:ss a")} depts={depts} numNotes={[]} vNotes={[]} iNotes={[]} entries={entries} callList={callList} personalDays={personalDays} holiDays={holiDays} type={user.firstname+' '+user.lastname+"'s Personal"} dateContext={dateContext} today={today} style={style} onDayClick={(e,day) => this.onDayClick(e,day)} user={this.props.user}/>} fileName={dateContext.format('MMMM')+dateContext.format('Y')+'pesonalsked.pdf'}>
-      					{({ blob, url, loading, error }) => (loading ? 'Loading document...' : <span onMouseOver={() => console.log('span hover')}>'Download as PDF'</span>)}
+					<Col id='downloadLink'><PDFDownloadLink onMouseOver={() => console.log('hover')} document={<MyDocument stamp={stamp} depts={depts} numNotes={[]} vNotes={[]} iNotes={[]} entries={entries} callList={callList} personalDays={personalDays} holiDays={holiDays} type={user.firstname+' '+user.lastname+"'s Personal"} dateContext={dateContext} today={today} style={style} onDayClick={(e,day) => this.onDayClick(e,day)} user={this.props.user}/>} fileName={dateContext.format('MMMM')+dateContext.format('Y')+'pesonalsked.pdf'}>
+      					{({ blob, url, loading, error }) => (loading ? 'Loading document...' : <span onMouseOver={this.hoverSpan}>Download as PDF</span>)}
     				</PDFDownloadLink></Col>
 				</div>
 
