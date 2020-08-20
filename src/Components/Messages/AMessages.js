8d1cc0616e5cf1ce40e1c5472bd151f66d640d9b
Noah Menikefs <nmenikefs@gmail.com>
2020-08-20 14:34:31 -0400

    push

--- src/Components/Messages/AMessages.js
+++ src/Components/Messages/AMessages.js
@@ -300,6 +300,7 @@ class AMessages extends React.Component{
 					<ListGroup key={j} horizontal>
 						<ListGroup.Item className='past list' action disabled>
 							You <span className='accepted'>accepted</span> {this.docIdToName(past[j].docid)}'s request for {this.entryIdToName(past[j].entryid)} {this.dateStyler(past[j].dates)}
+							<Button onClick={() => this.toggleShow(pends[n].id)} className="deny" size="sm" variant="danger">Delete</Button>
 						</ListGroup.Item>
 						<ListGroup.Item className='edates list'>{past[j].stamp}</ListGroup.Item>
 					</ListGroup>
@@ -310,6 +311,7 @@ class AMessages extends React.Component{
 					<ListGroup key={j} horizontal>
 						<ListGroup.Item className='past list' action onClick={() => this.toggleDShow(past[j].msg)}>
 							You <span className='denied'>denied</span> {this.docIdToName(past[j].docid)}'s request for {this.entryIdToName(past[j].entryid)} {this.dateStyler(past[j].dates)}
+							<Button onClick={() => this.toggleShow(pends[n].id)} className="deny" size="sm" variant="danger">Delete</Button>
 						</ListGroup.Item>
 						<ListGroup.Item className='edates list'>{past[j].stamp}</ListGroup.Item>
 					</ListGroup>
