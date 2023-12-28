34efb19b04e9cc454d2acf3ad642364aeedd4a4c
noah-menikefs <nmenikefs@gmail.com>
2023-12-28 18:49:22 -0500

    j

--- src/Components/PDF/MyDocument.js
+++ src/Components/PDF/MyDocument.js
@@ -25,7 +25,7 @@ const styles = StyleSheet.create({
     width: "auto", 
     borderStyle: "dashed", 
     borderWidth: 0.75, 
-    //borderRightWidth: 0, 
+    borderRightWidth: 0, 
     borderBottomWidth: 0,
     borderLeftWidth: 0
   }, 
